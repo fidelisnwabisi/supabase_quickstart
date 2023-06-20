b5e61b962ad451ad139e84ac263b6f0ed2b3b09e
fidelis <fidelisnwabisi@gmail.com>
2023-06-20 03:55:38 +0100

    added splash_page.dart

---
+++ lib/page/splash_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_quickstart/main.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    await Future.delayed(Duration.zero);
+    final session = supabase.auth.currentSession;
+
+    if (!mounted) if (session != null) {
+      Navigator.of(context).pushReplacementNamed("/account");
+    } else {
+      Navigator.of(context).pushReplacementNamed("/login");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
